9e1658e7cce15ca26fc0a8d3e6cd8b396b3bc5dc
Guilherme <guilherme.santoss1802@gmail.com>
2025-01-16 23:15:31 -0300

    alterações finais, mudanças de substituição de média por mediana em PA D,PA S e FC

--- scripts/preprocessamento.py
+++ scripts/preprocessamento.py
@@ -57,23 +57,3 @@ def substituir(df, coluna, valor_original, valor_novo):
     x = df[f"{coluna}"] == valor_original
 
     df.loc[x, f"{coluna}"] = valor_novo
-
-
-def classificar_ppa_completo(pas_value, pad_value):
-
-    if 60 <= pas_value <= 125 and 35 <= pad_value <= 80:
-        return "Normal"
-    elif 105 <= pas_value <= 135 and 50 <= pad_value <= 90:
-        return "Pre-Hipertensão PAS"
-    elif 80 <= pas_value <= 140 and 55 <= pad_value <= 85:
-        return "Pre-Hipertensão PAD"
-    elif 110 <= pas_value <= 150 and 40 <= pad_value <= 120:
-        return "HAS-1 PAS"
-    elif 90 <= pas_value <= 130 and 60 <= pad_value <= 90:
-        return "HAS-1 PAD"
-    elif 110 <= pas_value <= 190 and 60 <= pad_value <= 100:
-        return "HAS-2 PAS"
-    elif 90 <= pas_value <= 120 and 65 <= pad_value <= 90:
-        return "HAS-2 PAD"
-
-    return "Não Calculado"
